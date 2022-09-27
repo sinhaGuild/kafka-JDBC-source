7c8c9f4108a9e2171f4dce3fd5b47bd134d161bc
sinhaGuild <anurag.r.sinha@gmail.com>
2022-09-27 20:14:10 +0000

    Added Mongodb Sink Configuration

--- sqlTest.sql
+++ sqlTest.sql
@@ -1,5 +1,3 @@
 SELECT table_schema, table_name FROM INFORMATION_SCHEMA.tables WHERE TABLE_SCHEMA != 'information_schema';
 
 SELECT * FROM demo.accounts LIMIT 5;
-
-insert into demo.accounts (id, first_name, last_name, username, company, created_date) values (21, 'Hamelin', 'Billy', 'Hamelin Billy', 'Erdman-Halvorson', '2018-09-16');
